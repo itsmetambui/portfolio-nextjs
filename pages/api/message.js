04359e35c5577eb63cf7e8b6e94d3154957f6696
Tam Bui <itsmetambui@gmail.com>
2023-03-23 19:58:17 +0700

    fix bug

--- pages/api/message.js
+++ pages/api/message.js
@@ -3,15 +3,10 @@ export default async function handler(req, res) {
     res.status(405).send({ message: "Only POST requests allowed" });
     return;
   }
-
-  console.log(req.body);
-  const data = JSON.parse(req.body);
-  console.log(data);
-
   const result = await axios.post(
     "https://hooks.slack.com/services/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",
     {
-      data,
+      data: req.body.data,
     }
   );
 
